f413337b82cb76c96a2d0881e6e23cac9c8c7687
BenitoSchwankhart <39334512+BenitoSchwankhart@users.noreply.github.com>
2018-12-14 10:23:33 +0100

    Localhost

--- src/main/java/clientServerConnection/client.java
+++ src/main/java/clientServerConnection/client.java
@@ -13,49 +13,24 @@ import java.io.*;
 
 public class client {
 public static void main(String[]args) throws UnknownHostException, IOException{
-      //Scanner input = new Scanner(System.in); --> For upcoming Scanner
+	
       int port = 9090;
-      String host= "192.168.2.107";
       byte[] answer; 
       byte[] sendMessage;
-      InetAddress address = InetAddress.getByName(host);
+      InetAddress address = InetAddress.getLocalHost();
 
       Socket socket= new Socket(address,port);
       OutputStream os = socket.getOutputStream();
-      
-      public String convertHexToString(String hex){
-
-    	  StringBuilder sb = new StringBuilder();
-    	  StringBuilder temp = new StringBuilder();
-    	  
-    	  //49204c6f7665204a617661 split into two characters 49, 20, 4c...
-    	  for( int i=0; i<hex.length()-1; i+=2 ){
-    		  
-    	      //grab the hex in pairs
-    	      String output = hex.substring(i, (i + 2));
-    	      //convert hex to decimal
-    	      int decimal = Integer.parseInt(output, 16);
-    	      //convert the decimal to character
-    	      sb.append((char)decimal);
-    		  
-    	      temp.append(decimal);
-    	  }
-    	  System.out.println("Decimal : " + temp.toString());
-    	  
-    	  return sb.toString();
-      }
 
       System.out.println("Start...");
       RmxCalls rmx = new RmxCalls();
       answer = rmx.Positivquittung;
-      //Server answer must be fixed
 
       sendMessage = answer;
       DataOutputStream bw = new DataOutputStream(os);
 	  bw.write(sendMessage);
       bw.flush();
-      System.out.println("Message sent to server: "+ String.format( "{0:X4}" , 
-    		  sendMessage));
+      System.out.println("Message sent to server: "+ Arrays.toString(sendMessage));
       
       InputStream is = socket.getInputStream();
       byte[] bytes = IOUtils.toByteArray(is);
