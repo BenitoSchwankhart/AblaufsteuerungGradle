30501fd8e8bf3649dd0646e66da0f1ac04ac41dc
BenitoSchwankhart <39334512+BenitoSchwankhart@users.noreply.github.com>
2018-12-19 16:54:04 +0100

    Revert "ok"
    This reverts commit 95a547d50b181e650bc3641c864d571fb6f08212.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.IOException;
 import java.net.UnknownHostException;
 
@@ -10,33 +10,30 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
 	@Override
 
 	public void start(Stage primaryStage) throws UnknownHostException, IOException {
-		//Checking if Server is connected
+		// Checking if Server is connected
 		ClientConnection c = new ClientConnection();
 		c.main(null);
 		RmxCalls rmx = new RmxCalls();
-		if(rmx.getVerbindung() == 2) {
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Test.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} 
-		catch(Exception e) {
-			e.printStackTrace();
-							}
-		}
-		else {
+		if (rmx.getVerbindung() == 2) {
+			try {
+				AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Test.fxml"));
+				Scene scene = new Scene(root, 400, 400);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
 			System.out.println("Keine Verbindung zum Server");
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
