51be425c7b4456a23dfead894c422f9e56203fa2
liamwolf <39092658+liamwolf@users.noreply.github.com>
2019-01-23 16:38:14 +0100

    Zug Namen werden jetzt erkannt

--- src/main/java/clientServerConnection/RmxCalls.java
+++ src/main/java/clientServerConnection/RmxCalls.java
@@ -8,93 +8,112 @@ import java.nio.charset.Charset;
 public class RmxCalls {
 
 	private final static Charset CHARSET = Charset.forName("UTF-8");
-	
-		//Konstanten des RMX-Protokolls
 
-		public static final byte HEADBYTE = 0x7c;
-		public static final byte RMXVERSION = 0x01;
+	// Konstanten des RMX-Protokolls
 
-		// Wichtige RMX-Default-Pakete bzw. statische Pakete
-		
-		public static final byte[] Positivquittung = new byte[] { HEADBYTE,
-				0x04, 0x00, 0x00 };
-		public static final byte[] UnknownOPCODE = new byte[] { HEADBYTE,
-				0x04, 0x0f, 0x00 };
-		public static final byte[] Initialisieren = new byte[] { HEADBYTE, 0x05,
-				0x03, 0x02, RMXVERSION };
-		public static final byte[] ZugInfo = new byte[] { HEADBYTE, 0x04, 0x08, 0x01 };
-		public static final byte[] Status = new byte[] { HEADBYTE, 0x03, 0x04 };
-		public static final byte[] PowerOn = new byte[] { HEADBYTE, 0x04, 0x03,
-				(byte) 128 };
-		public static final byte[] PowerOff = new byte[] { HEADBYTE, 0x04,
-				0x03, 0x40 };
-		public static final byte[] Notfall = new byte[] { HEADBYTE, 0x04, 0x03,
-				0x08 };
+	public static final byte HEADBYTE = 0x7c;
+	public static final byte RMXVERSION = 0x01;
 
-		//Konstanten des Verbindungsstatus
-		
-		private static final int NULL = 0;
-		private static final int CONNECTING = 1;
-		private static final int CONNECTED = 2;
-		private static final int DISCONNECTING = 3;
-		private static final int DISCONNECTED = 4;
-		
-		private static int Verbindung = DISCONNECTED;
-		
-		public int getVerbindung() {
-			return Verbindung;
-		}
-		
-		public int setVerbindungNULL() {
-			return Verbindung = NULL;
-		}
-		public int setVerbindungCONNECTING() {
-			return Verbindung = CONNECTING;
-		}
-		public int setVerbindungCONNECTED() {
-			return Verbindung = CONNECTED;
-		}
-		public int setVerbindungDISCONNECTING() {
-			return Verbindung = DISCONNECTING;
-		}
-		public int setVerbindungDISCONNECTED() {
-			return Verbindung = DISCONNECTED;
-		}
-		
-		//Konverter:
-		
-		/**
-		 * Dezimalen int-Wert in 2 Bytes
-		 * (highByte und lowByte)
-		 * 
-		 * @param value
-		 * @return byte[] - Byte-Array (highByte/lowByte)
-		 */
-		protected static byte[] intToBytes(int value) {
-			byte[] byteValue = new byte[2];
-		
-			byteValue[1] = (byte) (value & 0xFF);
-			byteValue[0] = (byte) ((value >> 8) & 0xFF);
-		
-			return byteValue;
-		}
-		
-		/** Array zu Hexadezimal(Hex32)
-		 * 
-		 * @param toPrint
-		 */
-		
+	// Wichtige RMX-Default-Pakete bzw. statische Pakete
+
+	public static final byte[] Positivquittung = new byte[] { HEADBYTE, 0x04, 0x00, 0x00 };
+	public static final byte[] UnknownOPCODE = new byte[] { HEADBYTE, 0x04, 0x0f, 0x00 };
+	public static final byte[] Initialisieren = new byte[] { HEADBYTE, 0x05, 0x03, 0x02, RMXVERSION };
+	public static final byte[] ZugInfo = new byte[] { HEADBYTE, 0x04, 0x08, 0x01 };
+	public static final byte[] Status = new byte[] { HEADBYTE, 0x03, 0x04 };
+	public static final byte[] PowerOn = new byte[] { HEADBYTE, 0x04, 0x03, (byte) 128 };
+	public static final byte[] PowerOff = new byte[] { HEADBYTE, 0x04, 0x03, 0x40 };
+	public static final byte[] Notfall = new byte[] { HEADBYTE, 0x04, 0x03, 0x08 };
+
+	// Konstanten des Verbindungsstatus
+
+	private static final int NULL = 0;
+	private static final int CONNECTING = 1;
+	private static final int CONNECTED = 2;
+	private static final int DISCONNECTING = 3;
+	private static final int DISCONNECTED = 4;
+
+	private static int Verbindung = DISCONNECTED;
+
+	public int getVerbindung() {
+		return Verbindung;
+	}
+
+	public int setVerbindungNULL() {
+		return Verbindung = NULL;
+	}
+
+	public int setVerbindungCONNECTING() {
+		return Verbindung = CONNECTING;
+	}
+
+	public int setVerbindungCONNECTED() {
+		return Verbindung = CONNECTED;
+	}
+
+	public int setVerbindungDISCONNECTING() {
+		return Verbindung = DISCONNECTING;
+	}
+
+	public int setVerbindungDISCONNECTED() {
+		return Verbindung = DISCONNECTED;
+	}
+
+	// Konverter:
+
+	/**
+	 * Konvertiert bytes[] zu hexabytes
+	 * 
+	 * @param toPrint
+	 */
 	public void Hexaprint(byte[] toPrint) {
 		for (int i = 0; i < toPrint.length; i++) {
 			System.out.print(String.format("0x%02X", ((byte) toPrint[i])) + " ");
+		}
+	}
+
+	/**
+	 * Dezimalen int-Wert in 2 Bytes (highByte und lowByte)
+	 * 
+	 * @param value
+	 * @return byte[] - Byte-Array (highByte/lowByte)
+	 */
+	protected static byte[] intToBytes(int value) {
+		byte[] byteValue = new byte[2];
+
+		byteValue[1] = (byte) (value & 0xFF);
+		byteValue[0] = (byte) ((value >> 8) & 0xFF);
+
+		return byteValue;
+	}
+
+	/**
+	 * Array zu Hexadezimal(Hex32)
+	 * 
+	 * @param toPrint
+	 */
+
+	public void ASCIIprint(byte[] bytes) throws UnsupportedEncodingException {
+		String string = new String(bytes, "ASCII");
+		String alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz|";
+		char[] alphabetArray = alphabetString.toCharArray();
+
+		for (int i = 7; i < string.length(); i++) {
+
+			char charsbby = string.charAt(i);
+
+			for (int j = 0; j < alphabetArray.length; j++) {
+				char c = alphabetArray[j];
+
+				if (charsbby == c) {
+					String fertig = "" + charsbby;
+					
+					System.out.print(fertig.replace("|", ""));
+
+				}
 			}
 		}
-	
-	public String ASCIIprint(byte[] bytes) throws UnsupportedEncodingException {
-		String test = new String(bytes,"ASCII");
-		System.out.print(test);
-		return test;
+
 	}
-		
-		
+
 }
