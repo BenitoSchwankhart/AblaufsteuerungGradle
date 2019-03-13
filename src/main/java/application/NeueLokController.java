0af0de5b5da8dbe39e3441e217cde2e14be91110
liamwolf <39092658+liamwolf@users.noreply.github.com>
2019-03-13 12:42:19 +0100

    Fahrstufen werden angezeigt

--- src/main/java/application/NeueLokController.java
+++ src/main/java/application/NeueLokController.java
@@ -33,14 +33,17 @@ public class NeueLokController implements Initializable {
 
 	@FXML
 	private JFXTextField Namebox;
+
 //Das ist der Spinner
 	@FXML
 	private Spinner<Integer> adressespinner;
+
 	@FXML
 	private Text TypBoxLabel;
 
 	@FXML
 	private ChoiceBox<String> TypBox;
+
 	// Das ist der Submit Button
 	@FXML
 	private JFXButton submit_btn;
@@ -99,9 +102,15 @@ public class NeueLokController implements Initializable {
 
 	}
 //SubmitButton Funktion
+
+	private void getChoice(ChoiceBox<String> TypBox) {
+		String Fahrstufen = TypBox.getValue();
+		System.out.print(Fahrstufen);
+	}
+
 	@FXML
 	void submit_neueLok(ActionEvent event) {
-
+		submit_btn.setOnAction(e -> getChoice(TypBox));
 	}
 
 }
