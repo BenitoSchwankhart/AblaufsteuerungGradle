e892006a0df7e0d1a2a72430c09b396300a440d6
liamwolf <39092658+liamwolf@users.noreply.github.com>
2019-03-21 11:29:46 +0100

    Name wird ausgegeben

--- src/main/java/application/NeueLokController.java
+++ src/main/java/application/NeueLokController.java
@@ -113,45 +113,41 @@ public class NeueLokController implements Initializable {
 		String Fahrstufen = TypBox.getValue();
 		int FS = Integer.parseInt(Fahrstufen);
 		System.out.println(FS);
+		String Nameboxtext = Namebox.getText();
+		System.out.println(Nameboxtext);
 		return FS;
 	}
 
-
-
-
 	@FXML
 	void submit_neueLok(ActionEvent event) {
-		
+
 		int sub = getChoice(TypBox);
 
 		ConnectionCalls c = new ConnectionCalls();
 		RmxCalls rmx = new RmxCalls();
 		byte OPMODE = 0;
-		//byte COUNT, byte ZUGNR, byte OPMODE, byte[] NAME
-		
-		//Test welche Fahrstufe in TextBox ausgwählt wurde
-		if ( sub == 14) {
+		// byte COUNT, byte ZUGNR, byte OPMODE, byte[] NAME
+
+		// Test welche Fahrstufe in TextBox ausgwählt wurde
+		if (sub == 14) {
 			OPMODE = 0x09;
 			System.out.println("FS14");
-		}
-		else if ( sub == 24) {
+		} else if (sub == 24) {
 			OPMODE = 0x0C;
 			System.out.println("FS24");
-		}
-		else if ( sub == 126) {
+		} else if (sub == 126) {
 			OPMODE = 0x0F;
 			System.out.println("FS126");
+		} else {
+			throw new IllegalArgumentException("Fahrstufenauswahl fehlgeschlagen!");
 		}
-		else {
-			throw new IllegalArgumentException ("Fahrstufenauswahl fehlgeschlagen!");
-		}
-		
-		/*byte[] NAME = new byte[20]; //Maximale Länge auf 20 festgelegt
-		for(int i=0; i<20; i++) {
-			NAME[i] = i;
-		}*/
-		//int COUNT = NAME.length; 
-		//c.ZugErstellen();
+
+		/*
+		 * byte[] NAME = new byte[20]; //Maximale Länge auf 20 festgelegt for(int i=0;
+		 * i<20; i++) { NAME[i] = i; }
+		 */
+		// int COUNT = NAME.length;
+		// c.ZugErstellen();
 		// byte COUNT, byte ZUGNR, byte OPMODE, byte[] NAME
 		byte[] NAME = null;
 		int COUNT = NAME.length;
