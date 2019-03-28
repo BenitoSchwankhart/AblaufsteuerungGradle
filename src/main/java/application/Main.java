e5929e3756890c8c4d4bc9f5dbebe5e03acd88b9
liamwolf <39092658+liamwolf@users.noreply.github.com>
2019-03-28 18:59:56 +0100

    Revert "Merge branch 'MAIN' of https://github.com/BenitoSchwankhart/AblaufsteuerungGradle into MAIN"
    This reverts commit 01663e75d4fc9cd0aa8e57b4755e9ba5b6b670dc, reversing
    changes made to 879b172970c909d269376502e9b9b8865c75b002.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,9 +3,7 @@ package application;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-import datenbank.CreateTable;
-import datenbank.InsertIntoTable;
-import datenbank.ReadFromTable;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -14,35 +12,29 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
+	
 
-public void start(Stage primaryStage) throws UnknownHostException, IOException {
+	public void start(Stage primaryStage) throws UnknownHostException, IOException {
 
 		//*Checking if Server is connected
 		/*clientConnection c = new clientConnection();
 		c.main(null);
 		RmxCalls rmx = new RmxCalls();
 		if (rmx.getVerbindung() == 2) {*/
-		CreateTable c = new CreateTable();
-		c.createAktuellZug();
-		c.createZugDB();
-		InsertIntoTable i = new InsertIntoTable();
-		i.zugData(222, "DEMO_ZUG", 14);
-		i.setAktuellZug(222, "DEMO_ZUG");
-		try {
+			try {
 				AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Home.fxml"));
 				Scene scene = new Scene(root, 900, 450);
 				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 				primaryStage.setScene(scene);
 				primaryStage.show();
-			}
-		catch (Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}
-
 		/*} else {
 		System.out.println("Keine Verbindung zum Server");
 	}*/
+
+	}
 
 	public static void main(String[] args) {
 		launch(args);
