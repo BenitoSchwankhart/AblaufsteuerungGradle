937b4c9425b6b82c290c40a7bd94e061890d2abc
liamwolf <39092658+liamwolf@users.noreply.github.com>
2019-03-28 10:26:00 +0100

    Kleine Änderung beim Import

--- src/main/java/application/TestController.java
+++ src/main/java/application/TestController.java
@@ -5,38 +5,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXPasswordField;
-import com.jfoenix.controls.JFXTextField;
-import com.jfoenix.controls.JFXToggleButton;
-import com.sun.glass.ui.ClipboardAssistance;
-import clientServerConnection.*;
-import datenbank.InsertIntoTable;
+
 import datenbank.ReadFromTable;
-import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
+
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
-import javafx.stage.Modality;
+
 import javafx.stage.Stage;
-import clientServerConnection.ConnectionCalls;
 
 public class TestController implements Initializable {
 
