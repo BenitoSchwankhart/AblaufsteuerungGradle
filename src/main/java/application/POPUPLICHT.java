fdaee95042fb0860c39efb8eccc42a7795b4ece9
liamwolf <39092658+liamwolf@users.noreply.github.com>
2019-03-30 23:24:30 +0100

    POPUP Licht verbessert

---
+++ src/main/java/application/POPUPLICHT.java
@@ -0,0 +1,44 @@
+package application;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSlider;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class POPUPLICHT {
+
+    @FXML
+    private JFXButton submit_btn;
+	@FXML
+	private JFXSlider start;
+
+	@FXML
+	private JFXSlider ende;
+	@FXML
+	private JFXButton ok_btn;
+	@FXML
+	private Text zeit;
+
+	@FXML
+	void ok(ActionEvent event) {
+		double s = start.getValue();
+		double e = ende.getValue();
+		int ergebnis = (int) (e - s);
+		if (ergebnis > 0) {
+		String numberAsString = Double.toString(ergebnis);
+		zeit.setText(numberAsString + "min");}
+		else {
+			zeit.setText("Wert negativ!");
+		}
+
+	}
+
+    @FXML
+    void submit(ActionEvent event) {
+    	Stage stage = (Stage) submit_btn.getScene().getWindow();
+		stage.close();
+    }
+}
